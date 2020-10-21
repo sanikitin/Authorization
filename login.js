2c893da7ab0320c7221fe4bbe7c0424b93a03e65
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2020-10-21 15:57:37 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,18 @@
-<script>
-        let login = "Админ";
-        let password = "Я главный";
-        let userLogin = prompt ("Введите логин");
-        if (login == userLogin) {
-            let userPassword = prompt ("Введите пароль");
-            if (userPassword == password) {
-                alert ("Здравствуйте Админ");
-            } else if (userPassword !== password) {
-                if (userPassword == null || userPassword == "" || userPassword == undefined) {
-                alert ("Отмена");
-                } else {
-                alert ("Неверный пароль");
-                }
-            }
-        } else if (userLogin == null || userLogin == "" || userLogin == undefined) {
+
+let login = "Админ";
+let password = "Я главный";
+let userLogin = prompt ("Введите логин");
+if (login == userLogin) {
+    let userPassword = prompt ("Введите пароль");
+    if (userPassword == password) {
+        alert ("Здравствуйте Админ");
+    } else if (userPassword !== password) {
+        if (userPassword == null || userPassword == "" || userPassword == undefined) {
             alert ("Отмена");
-        } else alert ("Вы кто такой?");
-</script>
+        } else {
+            alert ("Неверный пароль");
+            }
+    }
+} else if (userLogin == null || userLogin == "" || userLogin == undefined) {
+    alert ("Отмена");
+} else alert ("Вы кто такой?");
